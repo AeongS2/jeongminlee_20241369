cb93f6628082107f7d4dce334339f6e3d8d10e83
AeongS2 <anestt@duksung.ac.kr>
2026-04-28 04:54:00 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
 
 // 아이템 데이터
 let dx, dy;
-let dsize = 5;
+let dsize = 50;
 let dActive = [];
 
 // 점수 데이터
@@ -21,11 +21,25 @@ let score = 0;
 let px, py;
 let pd = 30;
 
+function setItem(){
+  let x = 500;
+  let y = 150;
+
+  //첫줄
+  for(let i = 0; i < 10; i++){
+    dx[i] = x;
+    dy[i] = y;
+    dActive[i] = true;
+
+    x += 50;
+  }
+}
+
 
 function draw() {
   background(img);
-  textSize(20);
-  text("점수: " + score, 10, 30);
+  textSize(50);
+  text("점수: " + score, 10, 50);
 
   // 팩맨
   if(keyIsDown(LEFT_ARROW)) px -= 3;
@@ -41,7 +55,7 @@ function draw() {
     
     if(dActive[i] === true){
       fill(255, 240, 31);
-      ellipse(dActive[i].x, dActive[i].y, dsize);
+      ellipse(dx[i], dy[i], dsize);
 
       
     }
