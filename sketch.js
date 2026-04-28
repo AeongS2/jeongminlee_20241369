132ddb0bf723eb865828e0c5253b22d3bc23e7bb
AeongS2 <anestt@duksung.ac.kr>
2026-04-28 13:51:32 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ function preload(){
 function setup() {
   createCanvas(2816, 1536);
   setItem();
+  
 }
 
 // 아이템 데이터
@@ -24,7 +25,36 @@ let px, py;
 let pd = 30;
 
 // 맵 데이터
-let tileSize = 100;
+let tileSize = 50;
+// let map = [
+//   [1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,1],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  
+// ];
+
+let map = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1],
+  [1,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  
+];
+
+function drawMap(){
+  let resX = 0;
+  let resY = 0;
+  for(let y = 0; y<map.length; y++){
+    for(let x = 0; x<map[y].length; x++){
+      if(map[y][x] === 1){
+        //stroke(50);
+        fill(255,0,0);
+        resX = x*tileSize + 380;
+        resY = y*tileSize + 90;
+        rect(resX, resY, tileSize, tileSize);
+      }
+    }
+  }
+}
 
 //수정필요
 function setItem(){
@@ -59,16 +89,16 @@ function setItem(){
 }
 
 
+
 function draw() {
   
-
   background(img);
   fill(255, 255, 0);
   textSize(50);
   text("점수: " + score, 10, 50);
 
   // 벽
-
+  drawMap();  //수정
 
   // 팩맨
   // if(keyIsDown(LEFT_ARROW)) px -= 3;
